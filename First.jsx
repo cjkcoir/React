02aedbf0dbd50eb0ea04d3dc4ce17b2edc794ae7
chellakarthikeyan <56931912+cjkcoir@users.noreply.github.com>
2024-02-10 17:41:22 +0530

    add via file

---
+++ First.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function First() {
+  return (
+    <div className="h1 text-danger ">Hello World</div>
+  )
+}
+
+export default First
